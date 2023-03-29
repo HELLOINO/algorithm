36d3ed6b0f315aab854dd93872ec6074b0b179c0
HELLOINO <116244564+HELLOINO@users.noreply.github.com>
2023-03-29 22:44:07 +0900

    [level 0] Title: 등수 매기기, Time: 5.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,28 @@
+function solution(score) {
+    var val = [];
+    
+    score.map(a => {
+        val.push(a[0] + a[1]);
+    });
+    
+    console.log(val);
+    
+    for(var i = 0; i < val.length; i++) {
+        var a = val[val.indexOf(Math.max(...val))];
+        var count = 0;
+        
+        for(var j = 0; j < val.length; j++) {
+            if(val[j] == a) {
+                val[j] = -(i + 1);
+                count++;
+            }
+        }
+        if(count != 0) i += count - 1;
+    }
+    
+    val.forEach((a,b) => {
+       val[b] = -a;
+    });
+ 
+    return val;
+}
